adf1db41491bddd7fac966eef5c11662143c3c10
Max Toth <maxjtoth@gmail.com>
2023-09-10 16:54:28 -0700

    .

--- script.js
+++ script.js
@@ -15,32 +15,24 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-// Helper function to update the active link based on the section in the viewport
-function updateActiveLink(entries, observer) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            const sectionId = entry.target.id;
-            const navLink = document.querySelector(`.nav-link[href="#${sectionId}"]`);
-            
-            // Remove the 'active-link' class from all links
-            document.querySelectorAll('.nav-link').forEach(link => {
-                link.classList.remove('active-link');
-            });
+// Function to highlight the active link based on scroll position
+function highlightActiveLink() {
+    const scrollPosition = window.scrollY;
 
-            // Add the 'active-link' class to the corresponding link
-            if (navLink) {
-                navLink.classList.add('active-link');
-            }
+    document.querySelectorAll('.section').forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionBottom = sectionTop + section.offsetHeight;
+
+        const navLink = document.querySelector(`a[href="#${section.id}"]`);
+
+        if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+            navLink.classList.add('active-link');
+        } else {
+            navLink.classList.remove('active-link');
         }
     });
 }
 
-// Set up Intersection Observers for each section
-document.querySelectorAll('.section').forEach(section => {
-    const observer = new IntersectionObserver(updateActiveLink, {
-        root: null,
-        threshold: 0.5, // Adjust this threshold as needed
-    });
-
-    observer.observe(section);
-});
+// Update the active link on scroll
+window.addEventListener('scroll', highlightActiveLink);
+window.addEventListener('load', highlightActiveLink); // Check on page load as well
