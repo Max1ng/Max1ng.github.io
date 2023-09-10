cfd1f17b2deea07134564c02f06da0ccacf0ccce
Max Toth <maxjtoth@gmail.com>
2023-09-10 16:45:20 -0700

    make navbar change color based on where in page

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// Helper function to check if an element is in the viewport
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
 // Add smooth scrolling to anchor links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
@@ -23,4 +34,16 @@ window.addEventListener('scroll', function () {
     } else {
         navbar.classList.remove('fixed-navbar');
     }
+
+    // Check which section is in the viewport and update the active link
+    document.querySelectorAll('.nav-link').forEach(link => {
+        const sectionId = link.getAttribute('href').substring(1);
+        const section = document.getElementById(sectionId);
+
+        if (section && isElementInViewport(section)) {
+            link.classList.add('active-link');
+        } else {
+            link.classList.remove('active-link');
+        }
+    });
 });
